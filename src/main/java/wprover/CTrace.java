1a930628bb1bb7a25a25152cf69431755f0d2de3
vadgoblin@arch <vadgoblin@github.com>
2026-09-10 11:37:19 +0200

    update draw methods in CTrace to use newer graphics api

--- src/main/java/wprover/CTrace.java
+++ src/main/java/wprover/CTrace.java
@@ -1,6 +1,8 @@
 package wprover;
 
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.DataInputStream;
@@ -131,7 +133,7 @@ public class CTrace extends CClass {
 
         for (int i = 0; i < Num; i++) {
             if (!dlns)
-                g2.fillOval(PX[i] - radius / 2, PY[i] - radius / 2, radius, radius);
+                fillEllipse(g2, PX[i] - radius / 2.0, PY[i] - radius / 2.0, radius, radius);
             if (dlns) {
                 if (oObj != null && oObj.get_type() == CClass.CIRCLE)
                     drawALN(PX[i], PY[i], PX[(i + 1) % Num], PY[(i + 1) % Num], g2);
@@ -150,7 +152,7 @@ public class CTrace extends CClass {
      * @param y1 the y coordinate of the second point
      * @param g2 the Graphics2D object
      */
-    public void drawALN(int x, int y, int x1, int y1, Graphics2D g2) {
+    public void drawALN(double x, double y, double x1, double y1, Graphics2D g2) {
 
         if ((x1 < -0 || x1 > 1000) && (x < -0 || x > 1000))
             return;
@@ -158,12 +160,12 @@ public class CTrace extends CClass {
         if ((y1 < -0 || y1 > 1000) && (y < -0 || y > 1000))
             return;
 
-        int dx = x - x1;
-        int dy = y - y1;
+        double dx = x - x1;
+        double dy = y - y1;
         if(dx > MAXLEN || dx < - MAXLEN || dy > MAXLEN || dy < -MAXLEN)
             return;
 
-        g2.drawLine(x, y, x1, y1);
+        drawLine(g2, x, y, x1, y1);
     }
 
     /**
@@ -470,5 +472,40 @@ public class CTrace extends CClass {
      */
     public int getPointSize() {
         return Num;
+    }
+
+
+    private static final Ellipse2D.Double REUSABLE_ELLIPSE = new Ellipse2D.Double();
+
+    /**
+     * Draws an ellipse to the argument graphics object
+     */
+    private static void drawEllipse(Graphics2D g2, double x, double y, double w, double h) {
+        REUSABLE_ELLIPSE.setFrame(x, y, w, h);
+        g2.draw(REUSABLE_ELLIPSE);
+    }
+
+    /**
+     * Fills an ellipse to the argument graphics object
+     */
+    private static void fillEllipse(Graphics2D g2, double x, double y, double w, double h) {
+        REUSABLE_ELLIPSE.setFrame(x, y, w, h);
+        g2.fill(REUSABLE_ELLIPSE);
+    }
+
+    private static final Line2D.Double REUSABLE_LINE = new Line2D.Double();
+
+    /**
+     * Draws a line to the argument graphics object
+     *
+     * @param g2 the Graphics2D instance to draw to
+     * @param x1 the x coorditante of the start point
+     * @param y1 the y coorditante of the start point
+     * @param x2 the x coorditante of the end point
+     * @param y2 the y coorditante of the end point
+     */
+    private static void drawLine(Graphics2D g2, double x1, double y1, double x2, double y2) {
+        REUSABLE_LINE.setLine(x1, y1, x2, y2);
+        g2.draw(REUSABLE_LINE);
     }
 }
